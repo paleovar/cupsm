5e02b59dba7be754ad45f643780cb062bc5c0caf
Nils Weitzel <nils.weitzel@uni-tuebingen.de>
2024-12-05 18:21:30 +0000

    Updated conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -26,10 +26,13 @@ author = 'Nils Weitzel, Muriel Racky, Laura Braschoss, Kira Rehfeld'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
+    'autoapi.extension'
    ]
+#extensions = [
+#    'sphinx.ext.autodoc',
+#    'sphinx.ext.viewcode',
+#    'sphinx.ext.napoleon'
+#   ]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
